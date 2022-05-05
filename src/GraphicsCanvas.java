9f25993b6f1845a5719c8fc9e84c1deac5a57dbe
Kostiantyn Milkevych <au704910@uni.au.dk>
2022-05-05 14:47:03 +0200

    Slightly refactored GraphicsCanvas class

--- src/GraphicsCanvas.java
+++ src/GraphicsCanvas.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 
+import javax.swing.plaf.DimensionUIResource;
+
 /**
  * A Graphics Canvas, used as the main viewport in the application.
  */
 public class GraphicsCanvas extends Canvas {
     
-    private int gridCellCountX = 40;
+    private int gridCellCountX = 30;
     private int gridCellCountY = 20;
 
     /**
@@ -14,24 +16,38 @@ public class GraphicsCanvas extends Canvas {
     public GraphicsCanvas() {
         super();
         setBackground(Color.LIGHT_GRAY);
-        setSize(600, 400);
+        Dimension preferredDimension = new DimensionUIResource(600, 400);
+        setPreferredSize(preferredDimension);
     }
 
     /**
      * Paints content to the canvas using specified graphics.
      */
     public void paint(Graphics g) {
-        g.setColor(Color.BLACK);
-        int gridSize = Math.min(this.getWidth()/gridCellCountX, this.getHeight()/gridCellCountY);
+        
+        paintGrid(g);
 
-        for (int x = 0; x < gridCellCountX; x++) {
+    }
+
+    /**
+     * Draws the grid to the screen.
+     * @param g - Graphics object to draw grid with.
+     */
+    private void paintGrid(Graphics g) {
+        g.setColor(Color.BLACK);
+
+        int gridSize = Math.min(this.getWidth()/gridCellCountX, this.getHeight()/gridCellCountY);
+        int xLength = gridSize * gridCellCountX;
+        int yLength = gridSize * gridCellCountY;
+
+        for (int x = 0; x < gridCellCountX + 1; x++) {
             int p = x * gridSize;
-            g.drawLine(p, 0, p, this.getHeight());
+            g.drawLine(p, 0, p, yLength);
         }
 
-        for (int y = 0; y < gridCellCountY; y++) {
+        for (int y = 0; y < gridCellCountY + 1; y++) {
             int p = y * gridSize;
-            g.drawLine(0, p, this.getWidth(), p);
+            g.drawLine(0, p, xLength, p);
         }
     }
 
